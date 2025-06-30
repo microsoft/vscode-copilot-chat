f9f88cd89e112ac090191654f430f6535dab892d
meganrogge <merogge@microsoft.com>
2025-06-30 10:46:15 -0400

    revert a change

--- src/extension/tools/node/allTools.ts
+++ src/extension/tools/node/allTools.ts
@@ -3,9 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import './applyPatchTool';
 import './codebaseTool';
-import './createDirectoryTool';
 import './createFileTool';
 import './docTool';
 import './editNotebookTool';
@@ -13,9 +11,7 @@ import './findFilesTool';
 import './findTestsFilesTool';
 import './findTextInFilesTool';
 import './getErrorsTool';
-import './getNotebookCellOutputTool';
 import './getSearchViewResultsTool';
-import './getTaskOutputTool';
 import './githubRepoTool';
 import './insertEditTool';
 import './installExtensionTool';
@@ -28,17 +24,21 @@ import './notebookSummaryTool';
 import './readFileTool';
 import './readProjectStructureTool';
 import './replaceStringTool';
+import './applyPatchTool';
 import './runInTerminalTool';
 import './runNotebookCellTool';
+import './getNotebookCellOutputTool';
 import './runTaskTool';
 import './runTestsTool';
 import './scmChangesTool';
 import './searchWorkspaceSymbolsTool';
-import './simpleBrowserTool';
 import './terminalStateTools';
 import './testFailureTool';
 import './thinkTool';
 import './usagesTool';
 import './userPreferencesTool';
 import './vscodeAPITool';
+import './simpleBrowserTool';
+import './createDirectoryTool';
 import './vscodeCmdTool';
+import './getTaskOutputTool';
