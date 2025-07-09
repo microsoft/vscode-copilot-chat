aa8efe4c9c4786afb80aa67b62dc1bb3cec7eae3
Matt Bierner <matb@microsoft.com>
2025-07-09 15:08:02 -0700

    Show more detailed status when using mix of repos with and without remote indexes
    If you have a mix of github and ado repos, and can only access some of them, we currently show a very simple "remotely index" or "not indexed" status. This change shows more detail about the remote index is being used for some parts of the project

--- src/platform/workspaceChunkSearch/node/codeSearchWorkspaceDiff.ts
+++ src/platform/workspaceChunkSearch/node/codeSearchWorkspaceDiff.ts
@@ -12,7 +12,7 @@ import { URI } from '../../../util/vs/base/common/uri';
 import { IInstantiationService } from '../../../util/vs/platform/instantiation/common/instantiation';
 import { LogExecTime } from '../../log/common/logExecTime';
 import { ILogService } from '../../log/common/logService';
-import { CodeSearchDiff, CodeSearchRepoInfo, CodeSearchRepoTracker, RepoStatus } from '../../remoteCodeSearch/node/codeSearchRepoTracker';
+import { CodeSearchDiff, CodeSearchRepoTracker, RepoEntry, RepoStatus } from '../../remoteCodeSearch/node/codeSearchRepoTracker';
 import { ISimulationTestContext } from '../../simulationTestContext/common/simulationTestContext';
 import { IWorkspaceFileIndex, shouldIndexFile } from './workspaceFileIndex';
 
@@ -24,7 +24,7 @@ enum RepoState {
 
 interface RepoDiffState {
 	state: RepoState;
-	readonly info: CodeSearchRepoInfo;
+	readonly info: RepoEntry;
 	readonly initialChanges: ResourceSet;
 }
 
@@ -138,7 +138,7 @@ export class CodeSearchWorkspaceDiffTracker extends Disposable {
 		return seenFiles;
 	}
 
-	private async openRepo(info: CodeSearchRepoInfo) {
+	private async openRepo(info: RepoEntry) {
 		this._repos.delete(info.repo.rootUri);
 
 		const repoEntry: RepoDiffState = {
@@ -151,11 +151,11 @@ export class CodeSearchWorkspaceDiffTracker extends Disposable {
 		this.refreshRepoDiff(repoEntry);
 	}
 
-	private closeRepo(info: CodeSearchRepoInfo) {
+	private closeRepo(info: RepoEntry) {
 		this._repos.delete(info.repo.rootUri);
 	}
 
-	private async tryGetDiffedIndexedFiles(info: CodeSearchRepoInfo): Promise<URI[] | undefined> {
+	private async tryGetDiffedIndexedFiles(info: RepoEntry): Promise<URI[] | undefined> {
 		const diff = await this.tryGetDiff(info);
 		if (!diff) {
 			return;
@@ -170,7 +170,7 @@ export class CodeSearchWorkspaceDiffTracker extends Disposable {
 		return Array.from(initialChanges);
 	}
 
-	private async tryGetDiff(repoInfo: CodeSearchRepoInfo): Promise<CodeSearchDiff | undefined> {
+	private async tryGetDiff(repoInfo: RepoEntry): Promise<CodeSearchDiff | undefined> {
 		return this._repoTracker.diffWithIndexedCommit(repoInfo);
 	}
 
