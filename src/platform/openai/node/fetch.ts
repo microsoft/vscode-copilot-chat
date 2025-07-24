2d990e578cdcc9b8f667dc8eb7fbabcc9a23d11b
Rob Lourens <roblourens@gmail.com>
2025-07-24 14:13:18 -0700

    Set max_tokens

--- src/platform/openai/node/fetch.ts
+++ src/platform/openai/node/fetch.ts
@@ -520,8 +520,7 @@ async function fetchWithInstrumentation(
 	const intent = locationToIntent(location);
 
 	if (request.prediction) {
-		delete request.prediction;
-		request.max_tokens = 50000;
+		request.max_tokens = 70000;
 	}
 
 	// Wrap the Promise with success/error callbacks so we can log/measure it
