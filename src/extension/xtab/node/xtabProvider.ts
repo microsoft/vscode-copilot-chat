9cdd42dd624d424bbe2e694fb58a737323e71ef0
BeniBenj <besimmonds@microsoft.com>
2025-09-23 16:06:31 +0200

    remove xtab get token

--- src/extension/xtab/node/xtabProvider.ts
+++ src/extension/xtab/node/xtabProvider.ts
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Raw } from '@vscode/prompt-tsx';
-import { IAuthenticationService } from '../../../platform/authentication/common/authentication';
 import { FetchStreamSource } from '../../../platform/chat/common/chatMLFetcher';
 import { ChatFetchError, ChatFetchResponseType, ChatLocation } from '../../../platform/chat/common/commonTypes';
 import { toTextParts } from '../../../platform/chat/common/globalStringUtils';
@@ -99,7 +98,6 @@ export class XtabProvider implements IStatelessNextEditProvider {
 		@ILanguageContextProviderService private readonly langCtxService: ILanguageContextProviderService,
 		@ILanguageDiagnosticsService private readonly langDiagService: ILanguageDiagnosticsService,
 		@IIgnoreService private readonly ignoreService: IIgnoreService,
-		@IAuthenticationService private readonly authenticationService: IAuthenticationService,
 	) {
 		this.delayer = new Delayer(this.configService, this.expService);
 		this.tracer = createTracer(['NES', 'XtabProvider'], (s) => this.logService.trace(s));
@@ -340,7 +338,6 @@ export class XtabProvider implements IStatelessNextEditProvider {
 		logContext.setPrompt(messages);
 		telemetryBuilder.setPrompt(messages);
 
-		const secretKey = (await this.authenticationService.getCopilotToken(undefined, true)).token;
 		await this.debounce(delaySession, telemetryBuilder);
 		if (cancellationToken.isCancellationRequested) {
 			return Result.error(new NoNextEditReason.GotCancelled('afterDebounce'));
@@ -364,9 +361,6 @@ export class XtabProvider implements IStatelessNextEditProvider {
 				shouldRemoveCursorTagFromResponse,
 				promptingStrategy: promptOptions.promptingStrategy,
 				retryState,
-			},
-			{
-				secretKey,
 			},
 			delaySession,
 			tracer,
@@ -472,9 +466,6 @@ export class XtabProvider implements IStatelessNextEditProvider {
 			shouldRemoveCursorTagFromResponse: boolean;
 			retryState: RetryState;
 		},
-		requestOptions: {
-			secretKey: string;
-		},
 		delaySession: DelaySession,
 		parentTracer: ITracer,
 		telemetryBuilder: StatelessNextEditTelemetryBuilder,
@@ -524,7 +515,6 @@ export class XtabProvider implements IStatelessNextEditProvider {
 				// max_tokens: 256, // `max_tokens` is not supported along with `prediction` - https://platform.openai.com/docs/guides/predicted-outputs#limitations
 				stream: true,
 				prediction,
-				secretKey: requestOptions.secretKey,
 			} satisfies OptionalChatRequestParams,
 			undefined,
 			{
