ace62e2df77be27f25e07b8ba7f18494b2928196
Joaquín Ruales <1588988+jruales@users.noreply.github.com>
2025-11-03 16:30:06 -0800

    Also update vscode run command

--- src/extension/context/node/resolvers/vscodeContext.ts
+++ src/extension/context/node/resolvers/vscodeContext.ts
@@ -109,8 +109,10 @@ export async function parseSettingsAndCommands(workbenchService: IWorkbenchServi
 				return parsedMetadata;
 			}
 			else {
-				const allcommands = (await workbenchService.getAllCommands(/* filterByPreCondition */true));
-				const commandItem = allcommands.find(commandItem => commandItem.command === item.details?.key);
+				// Get all commands regardless of preconditions, because there are some commands that a user may meet the preconditions for,
+				// but Copilot not, so we still will show the command in the palette for the user to run.
+				const allCommandsNoFilter = (await workbenchService.getAllCommands(/* filterByPreCondition */false));
+				const commandItem = allCommandsNoFilter.find(commandItem => commandItem.command === item.details?.key);
 				if (!commandItem) {
 					// If we can't find the command on the list, just open the command palette without any pre-filled filter
 					parsedMetadata.push({
