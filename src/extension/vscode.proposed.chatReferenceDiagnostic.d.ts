000177bb3a118fbba1ffd81699ad8149a809ac61
Don Jayamanne <don.jayamanne@outlook.com>
2025-11-27 16:36:38 +1100

    Update chatReferendceDiagnostic.d.ts from core

--- src/extension/vscode.proposed.chatReferenceDiagnostic.d.ts
+++ src/extension/vscode.proposed.chatReferenceDiagnostic.d.ts
@@ -9,7 +9,7 @@ declare module 'vscode' {
 		/**
 		 * The value of this reference. The `string | Uri | Location` types are used today, but this could expand in the future.
 		 */
-		readonly value: string | Uri | Location | DiagnosticCollection | unknown;
+		readonly value: string | Uri | Location | ChatReferenceDiagnostic | unknown;
 	}
 
 	export class ChatReferenceDiagnostic {
