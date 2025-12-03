d35f76ce58fbb1ac3532d4cde8e721dc828f8fed
meigaoms <xuga@MacBook-Pro-2.local>
2025-12-03 13:53:19 -0800

    Update test for the new telemetry adding

--- src/extension/test/node/utils.spec.ts
+++ src/extension/test/node/utils.spec.ts
@@ -292,8 +292,17 @@ suite('EditSurvivalTracker', () => {
 				'x-editor': 'edit-editor'
 			})),
 			{
-				'fourGram': 0.75,
-				'noRevert': 1,
+				"fourGram": 0.75,
+				"noRevert": 1,
+				textAfterAiEdits: [
+					'\r\n\r\n\t\tif (!args) {\r\n\t\t\tthrow new Error("invalid json document!");\r\n\t\t}'
+				],
+				textAfterUserEdits: [
+					'\r\n\r\n\t\tif (!args) {\r\n\t\t\tthrow new Error("");\r\n\t\t}'
+				],
+				textBeforeAiEdits: [
+					'\r\n'
+				]
 			}
 		);
 	});
