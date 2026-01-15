0845cd92a3af97ee0a83ebef1df9efdd7edba2df
ulugbekna <ulugbekna@gmail.com>
2026-01-15 11:10:25 +0100

    fix incorrect import

--- src/extension/completions-core/vscode-node/lib/src/ghostText/ghostText.ts
+++ src/extension/completions-core/vscode-node/lib/src/ghostText/ghostText.ts
@@ -2,9 +2,9 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { IExperimentationService } from '../../../../../../lib/node/chatLibMain';
 import { CopilotNamedAnnotationList } from '../../../../../../platform/completions-core/common/openai/copilotAnnotations';
 import { ConfigKey as ChatConfigKey, IConfigurationService } from '../../../../../../platform/configuration/common/configurationService';
+import { IExperimentationService } from '../../../../../../platform/telemetry/common/nullExperimentationService';
 import { ITelemetryService } from '../../../../../../platform/telemetry/common/telemetry';
 import { createSha256Hash } from '../../../../../../util/common/crypto';
 import { generateUuid } from '../../../../../../util/vs/base/common/uuid';
