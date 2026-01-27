4de9bd554e29cc61d9a83ee8d26dccbd3e60bd0a
Karthik Nadig <kanadig@microsoft.com>
2026-01-26 16:37:26 -0800

    Apply suggestion from @karthiknadig

--- src/extension/tools/vscode-node/askQuestionsTool.ts
+++ src/extension/tools/vscode-node/askQuestionsTool.ts
@@ -159,7 +159,7 @@ export class AskQuestionsTool implements ICopilotTool<IAskQuestionsParams> {
 					value: opt.label
 				})),
 				defaultValue,
-				allowFreeformInput: question.allowFreeFormInput
+				allowFreeformInput: question.allowFreeformInput
 			}
 		);
 	}
