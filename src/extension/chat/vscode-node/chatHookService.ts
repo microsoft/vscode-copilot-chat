7d276a16395621b90270e6cafd4663ec919854df
Rob Lourens <roblourens@gmail.com>
2026-02-03 16:13:27 -0800

    API version

--- src/extension/chat/vscode-node/chatHookService.ts
+++ src/extension/chat/vscode-node/chatHookService.ts
@@ -10,6 +10,7 @@ export class ChatHookService implements IChatHookService {
 	declare readonly _serviceBrand: undefined;
 
 	async executeHook(hookType: vscode.ChatHookType, options: vscode.ChatHookExecutionOptions, token?: vscode.CancellationToken): Promise<vscode.ChatHookResult[]> {
-		return vscode.chat.executeHook(hookType, options, token);
+		// Just be nice to vscode devs with this API change
+		return vscode.chat.executeHook?.(hookType, options, token);
 	}
 }
