f606d8032f4c67cc1565177c13fa1a63a3944435
Don Jayamanne <don.jayamanne@outlook.com>
2026-02-17 10:18:35 +1100

    Adopt the latest github copilot npm

--- src/extension/agents/copilotcli/node/copilotcliSessionService.ts
+++ src/extension/agents/copilotcli/node/copilotcliSessionService.ts
@@ -67,6 +67,21 @@ export const ICopilotCLISessionService = createServiceIdentifier<ICopilotCLISess
 
 const SESSION_SHUTDOWN_TIMEOUT_MS = 300 * 1000;
 
+class NoopTelemetryService {
+	constructor(protected authManager?: unknown) { }
+	sendTelemetryEvent(eventName: string, properties?: unknown, measurements?: unknown, tags?: unknown) {
+		//
+	}
+	sendHydroEvent(event: unknown) {
+		//
+	}
+	dispose() {
+		//
+	}
+}
+
+type TelemetryService = ConstructorParameters<typeof internal.LocalSessionManager>[0]['telemetryService'];
+
 export class CopilotCLISessionService extends Disposable implements ICopilotCLISessionService {
 	declare _serviceBrand: undefined;
 
@@ -98,7 +113,8 @@ export class CopilotCLISessionService extends Disposable implements ICopilotCLIS
 		this.monitorSessionFiles();
 		this._sessionManager = new Lazy<Promise<internal.LocalSessionManager>>(async () => {
 			const { internal } = await this.copilotCLISDK.getPackage();
-			return new internal.LocalSessionManager({});
+			const telemetryService = new NoopTelemetryService() as unknown as TelemetryService;
+			return new internal.LocalSessionManager({ telemetryService, flushDebounceMs: undefined, settings: undefined, version: undefined });
 		});
 		this._sessionTracker = this.instantiationService.createInstance(CopilotCLISessionWorkspaceTracker);
 	}
