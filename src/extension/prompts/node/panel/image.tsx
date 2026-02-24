d530d04f86751efc4011f01ea4debdb107995fb6
Justin Chen <54879025+justschen@users.noreply.github.com>
2026-02-23 18:23:59 -0800

    Revert "make sure all copilot chat types get github uploads (#3824)"
    This reverts commit 8f419ebcf7add2ce553f8f4113d67dbcba9dd528.

--- src/extension/prompts/node/panel/image.tsx
+++ src/extension/prompts/node/panel/image.tsx
@@ -92,9 +92,9 @@ export class Image extends PromptElement<ImageProps, unknown> {
 			const variable = await this.props.variableValue;
 			let imageSource = Buffer.from(variable).toString('base64');
 			let imageMimeType: string | undefined = undefined;
-			const isChatRequest = typeof this.promptEndpoint.urlOrRequestMetadata !== 'string' && (this.promptEndpoint.urlOrRequestMetadata.type === RequestType.ChatCompletions || this.promptEndpoint.urlOrRequestMetadata.type === RequestType.ChatResponses || this.promptEndpoint.urlOrRequestMetadata.type === RequestType.ChatMessages);
+			const isChatCompletions = typeof this.promptEndpoint.urlOrRequestMetadata !== 'string' && this.promptEndpoint.urlOrRequestMetadata.type === RequestType.ChatCompletions;
 			const enabled = this.configurationService.getExperimentBasedConfig(ConfigKey.EnableChatImageUpload, this.experimentationService);
-			if (isChatRequest && enabled && modelCanUseImageURL(this.promptEndpoint)) {
+			if (isChatCompletions && enabled && modelCanUseImageURL(this.promptEndpoint)) {
 				try {
 					const githubToken = (await this.authService.getGitHubSession('any', { silent: true }))?.accessToken;
 					const mimeType = getMimeType(imageSource) ?? imageMimeType;
