f7684086a341fb773330632c0d9c8f3952e3e5a0
Paul Wang <paul_wang347@hotmail.com>
2026-03-13 11:05:52 -0700

    feature flag

--- src/extension/trajectory/vscode-node/chatDebugLogProvider.ts
+++ src/extension/trajectory/vscode-node/chatDebugLogProvider.ts
@@ -6,11 +6,13 @@
 import { Raw } from '@vscode/prompt-tsx';
 import * as vscode from 'vscode';
 import { ChatFetchResponseType } from '../../../platform/chat/common/commonTypes';
+import { ConfigKey, IConfigurationService } from '../../../platform/configuration/common/configurationService';
 import { ILogService } from '../../../platform/log/common/logService';
 import { messageToMarkdown } from '../../../platform/log/common/messageStringify';
 import { isOpenAiFunctionTool } from '../../../platform/networking/common/fetch';
 import { IOTelService, type ICompletedSpanData } from '../../../platform/otel/common/otelService';
 import { IRequestLogger, LoggedInfoKind, LoggedRequestKind, type LoggedRequest } from '../../../platform/requestLogger/node/requestLogger';
+import { IExperimentationService } from '../../../platform/telemetry/common/nullExperimentationService';
 import { ITrajectoryLogger, ITrajectoryStep } from '../../../platform/trajectory/common/trajectoryLogger';
 import { Disposable } from '../../../util/vs/base/common/lifecycle';
 import { generateUuid } from '../../../util/vs/base/common/uuid';
@@ -576,6 +578,8 @@ export class ChatDebugLogProviderContribution extends Disposable implements IExt
 		@ILogService private readonly _logService: ILogService,
 		@IRequestLogger private readonly _requestLogger: IRequestLogger,
 		@IOTelService private readonly _otelService: IOTelService,
+		@IConfigurationService private readonly _configurationService: IConfigurationService,
+		@IExperimentationService private readonly _experimentationService: IExperimentationService,
 	) {
 		super();
 
@@ -591,6 +595,10 @@ export class ChatDebugLogProviderContribution extends Disposable implements IExt
 				spans.push(span);
 			}
 		}));
+
+		if (!this._configurationService.getExperimentBasedConfig(ConfigKey.AgentDebugLogEnabled, this._experimentationService)) {
+			return;
+		}
 
 		if (typeof vscode.chat?.registerChatDebugLogProvider !== 'function') {
 			this._logService.info('[ChatDebugLogProvider] Chat debug API not available, skipping registration');
