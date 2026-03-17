0762dabaae7dee0f0a586d8a8afd2687169fed2b
Anisha Agarwal <anisagarwal@microsoft.com>
2026-03-16 19:51:11 -0700

    remove logging

--- src/extension/prompt/node/searchSubagentToolCallingLoop.ts
+++ src/extension/prompt/node/searchSubagentToolCallingLoop.ts
@@ -135,7 +135,6 @@ export class SearchSubagentToolCallingLoop extends ToolCallingLoop<ISearchSubage
 
 	protected async fetch({ messages, finishedCb, requestOptions }: ToolCallingLoopFetchOptions, token: CancellationToken): Promise<ChatResponse> {
 		const endpoint = await this.getEndpoint();
-		this._logService.info(`[SearchSubagent] Prompt messages:\n${JSON.stringify(messages, undefined, 2)}`);
 		const response = await endpoint.makeChatRequest2({
 			debugName: SearchSubagentToolCallingLoop.ID,
 			messages,
@@ -155,7 +154,6 @@ export class SearchSubagentToolCallingLoop extends ToolCallingLoop<ISearchSubage
 			},
 			requestKindOptions: { kind: 'subagent' }
 		}, token);
-		this._logService.info(`[SearchSubagent] Response:\n${JSON.stringify(response, undefined, 2)}`);
 		return response;
 	}
 }
