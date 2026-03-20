88dd4259503da73071600156e15003152e6f0452
Logan Ramos <loganramos@microsoft.com>
2026-03-20 16:08:50 -0400

    Circular dependency

--- src/platform/fetch/node/newFetchServiceImpl.ts
+++ src/platform/fetch/node/newFetchServiceImpl.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { IInstantiationService } from '../../../util/vs/platform/instantiation/common/instantiation';
 import { ILogService } from '../../log/common/logService';
 import { IFetcherService } from '../../networking/common/fetcherService';
 import { IExperimentationService } from '../../telemetry/common/nullExperimentationService';
@@ -11,10 +12,17 @@ import { BaseNewFetchService } from '../common/newFetchService';
 export class NewFetchServiceImpl extends BaseNewFetchService {
 	constructor(
 		@IFetcherService fetcherService: IFetcherService,
-		@IExperimentationService experimentationService: IExperimentationService,
+		@IInstantiationService instantiationService: IInstantiationService,
 		@ILogService logService: ILogService,
 	) {
-		super(fetcherService, experimentationService, {
+		// Use a lazy proxy for IExperimentationService to break the circular dependency:
+		// ICAPIClientService -> INewFetchService -> IExperimentationService -> ITelemetryService -> IDomainService -> ICAPIClientService
+		// The experimentation service is only called at fetch time, not during construction.
+		const lazyExperimentation = {
+			getTreatmentVariable: <T extends boolean | number | string>(name: string) =>
+				instantiationService.invokeFunction(accessor => accessor.get(IExperimentationService)).getTreatmentVariable<T>(name),
+		};
+		super(fetcherService, lazyExperimentation, {
 			logger: logService,
 			circuitBreaker: {},
 		});
