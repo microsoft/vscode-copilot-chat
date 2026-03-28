9bd44fefd86cf146e5ce093a5bc4e61e6645874b
Zhichao Li <zhichli@microsoft.com>
2026-03-27 17:20:13 -0700

    feat: wire OTel survival events into apply_patch, replace_string, and code_mapper tools

--- src/extension/prompts/node/codeMapper/codeMapperService.ts
+++ src/extension/prompts/node/codeMapper/codeMapperService.ts
@@ -12,8 +12,9 @@ import { IEditSurvivalTrackerService, IEditSurvivalTrackingSession } from '../..
 import { IFileSystemService } from '../../../../platform/filesystem/common/fileSystemService';
 import { inferAlternativeNotebookContentFormat } from '../../../../platform/notebook/common/alternativeContent';
 import { IAlternativeNotebookContentEditGenerator, NotebookEditGenrationSource } from '../../../../platform/notebook/common/alternativeContentEditGenerator';
-import { INotebookService } from '../../../../platform/notebook/common/notebookService';
-import { ITelemetryService } from '../../../../platform/telemetry/common/telemetry';
+import { INotebookService } from '../../../../platform/notebook/common/notebookService'; import { emitEditSurvivalEvent } from '../../../platform/otel/common/genAiEvents';
+import { GenAiMetrics } from '../../../platform/otel/common/genAiMetrics';
+import { IOTelService } from '../../../platform/otel/common/otelService'; import { ITelemetryService } from '../../../../platform/telemetry/common/telemetry';
 import { IWorkspaceService } from '../../../../platform/workspace/common/workspaceService';
 import { findNotebook } from '../../../../util/common/notebooks';
 import { createServiceIdentifier } from '../../../../util/common/services';
@@ -101,6 +102,7 @@ class DocumentCodeMapper extends Disposable implements ICodeMapperService {
 		@ITelemetryService private readonly _telemetryService: ITelemetryService,
 		@IEditSurvivalTrackerService private readonly _editSurvivalTrackerService: IEditSurvivalTrackerService,
 		@IFileSystemService private readonly _fileSystemService: IFileSystemService,
+		@IOTelService private readonly _otelService: IOTelService,
 	) {
 		super();
 		this.codeMapper = this.instantiationService.createInstance(CodeMapper);
@@ -138,7 +140,7 @@ class DocumentCodeMapper extends Disposable implements ICodeMapperService {
 		const result = await mapCode(request, responseStream, documentContext, this.codeMapper, this._telemetryService, telemetryInfo, token);
 		const telemetry = result?.telemetry;
 		if (telemetry) {
-			editSurvivalTracker?.startReporter(res => reportEditSurvivalEvent(res, telemetry));
+			editSurvivalTracker?.startReporter(res => reportEditSurvivalEvent(res, telemetry, this._otelService));
 		}
 		return result;
 	}
@@ -291,7 +293,7 @@ function spyResponseStream(responseStream: vscode.MappedEditsResponseStream, cal
 	};
 }
 
-function reportEditSurvivalEvent(res: EditSurvivalResult, { requestId, speculationRequestId, requestSource, mapper, chatRequestModel }: CodeMapperOutcomeTelemetry) {
+function reportEditSurvivalEvent(res: EditSurvivalResult, { requestId, speculationRequestId, requestSource, mapper, chatRequestModel }: CodeMapperOutcomeTelemetry, otelService: IOTelService) {
 
 	/* __GDPR__
 		"codeMapper.trackEditSurvival" : {
@@ -341,4 +343,7 @@ function reportEditSurvivalEvent(res: EditSurvivalResult, { requestId, speculati
 		survivalRateFourGram: res.fourGram,
 		survivalRateNoRevert: res.noRevert,
 	});
+
+	emitEditSurvivalEvent(otelService, 'code_mapper', res.fourGram, res.noRevert, res.timeDelayMs, res.didBranchChange, requestId ?? '');
+	GenAiMetrics.recordEditSurvivalRate(otelService, 'code_mapper', res.fourGram, res.timeDelayMs);
 }
